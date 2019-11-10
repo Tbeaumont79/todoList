d563a0511fb1b144f091c705f0b17b2ace2c9944
tbeaumont <tbeaumont79@icloud.com>
2019-11-10 12:08:13 +0100

    adding the link between the routes and the app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require('mongoose')
 const app = express();
-
+const todoElement = require('./routes/todoElement')
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -36,18 +36,7 @@ const day = date.getDate();
 
 });
 
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
+app.post("/", todoElement);
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
